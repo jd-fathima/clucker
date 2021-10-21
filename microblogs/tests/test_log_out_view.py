9c911a0a63f8bab22e32d383bee5733cafeb953b
Fathima Jamal-Deen <fathima.jamal-deen@kcl.ac.uk>
2021-10-21 21:35:47 +0100

    Remove the failing test experiment

--- microblogs/tests/test_log_out_view.py
+++ microblogs/tests/test_log_out_view.py
@@ -17,9 +17,6 @@ class LogOutViewTestCase(TestCase, LogInTester):
         password='Password123',
         is_active = True,
     )
-    
-    def test_breaking_the_tests(self):
-        self.fail("This test is made to fail!!")
 
     def test_log_out_url(self):
         self.assertEqual(self.url, '/log_out/')
