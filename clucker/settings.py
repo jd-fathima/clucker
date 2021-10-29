bc43f412d12c29bea64101c0b007009814becbda
Fathima Jamal-Deen <fathima.jamal-deen@kcl.ac.uk>
2021-10-29 21:58:26 +0100

    Render messages in all content pages

--- clucker/settings.py
+++ clucker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from django.contrib.messages import constants as message_constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +133,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #User model for authentication purposes
 AUTH_USER_MODEL = 'microblogs.User'
 POST_MODEL = 'microblogs.Post'
+
+#Message level tags should use Bootstrap terms
+MESSAGE_TAGS={
+    message_constants.DEBUG: 'dark',
+    message_constants.ERROR: 'danger'
+}
