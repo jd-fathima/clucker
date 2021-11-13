233fc6c6af9e30134a2639c5fd839760464e9b82
Fathima Jamal-Deen <fathima.jamal-deen@kcl.ac.uk>
2021-11-13 15:05:13 +0000

    Make sign_up view @login_prohibited

--- microblogs/tests/views/test_sign_up_view.py
+++ microblogs/tests/views/test_sign_up_view.py
@@ -10,6 +10,8 @@ from microblogs.tests.helpers import LogInTester
 class SignUpViewTestCase(TestCase, LogInTester):
     """Tests of the sign up view """
 
+    fixtures = ['microblogs/tests/fixtures/default_user.json']
+
     def setUp(self):
         self.url = reverse('sign_up')
         self.form_input = {
@@ -21,6 +23,7 @@ class SignUpViewTestCase(TestCase, LogInTester):
             'new_password':'Password123',
             'password_confirmation':'Password123'
         }
+        self.user = User.objects.get(username='@johndoe')
 
     def test_sign_up_url(self):
         self.assertEqual(self.url, '/sign_up/')
@@ -33,6 +36,14 @@ class SignUpViewTestCase(TestCase, LogInTester):
         form = response.context['form']
         self.assertTrue(isinstance(form, SignUpForm))
         self.assertFalse(form.is_bound)
+
+    def test_get_sign_up_redirects_when_logged_in(self):
+        self.client.login(username = self.user.username, password = "Password123")
+        response = self.client.get(self.url, follow=True)
+        redirect_url = reverse('feed')
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'feed.html')
+
 
     def test_unsuccessful_sign_up(self):
         self.form_input['username']= 'BAD_USERNAME'
@@ -63,3 +74,13 @@ class SignUpViewTestCase(TestCase, LogInTester):
         is_password_correct = check_password('Password123', user.password)
         self.assertTrue(is_password_correct)
         self.assertTrue(self._is_logged_in())
+
+    def test_post_sign_up_redirects_when_logged_in(self):
+        self.client.login(username = self.user.username, password = "Password123")
+        before_count = User.objects.count() #number of users before count
+        response = self.client.post(self.url, self.form_input, follow=True)
+        after_count = User.objects.count()
+        self.assertEqual(after_count, before_count)
+        redirect_url = reverse('feed')
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'feed.html')
