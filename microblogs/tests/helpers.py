eb58135f90890597e20496bb8d1a0cc4fea767ad
Fathima Jamal-Deen <fathima.jamal-deen@kcl.ac.uk>
2021-11-13 13:20:25 +0000

    Implement redirect to login when accessing user_list
    -Make user_list view @login_requried
    Redirect successful login to next page if provided

--- microblogs/tests/helpers.py
+++ microblogs/tests/helpers.py
@@ -1,3 +1,10 @@
+from django.urls import reverse
+
+def reverse_with_next(url_name, next_url):
+    url = reverse(url_name)
+    url += f"?next={next_url}"
+    return url
+
 class LogInTester:
 #check if user is logged in
     def _is_logged_in(self):
